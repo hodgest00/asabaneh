50fb8cf749dd9bcebc2cc7e31833cca0fba21a2b
hodgest <hodgest.1961@gmail.com>
2023-03-28 15:49:13 +0200

    firstCommit

---
+++ variables.js
@@ -0,0 +1,22 @@
+let firstName = "Cumali";
+let lastName = "Kavaklı";
+let country = "Almanya" ;
+let age = "32";
+let isMarried = false;
+
+// Decalaring variables with number values
+//^ Javascript te ondalıklı sayılar ve tam sayılar number veri türündedir. 
+
+const gravity = 9.81 // number - earth gravity
+const bollingPoint = 100 //number- water boiling point 
+
+//^ Değişkenler 1 satırda "," le ayrımış şekilde tanımlanabilirler.
+
+let name =" Cumali Kavaklı",
+    job = "Teacher",
+    live ="Spain";
+
+console.log(name,",", job, ",", live);
+console.log("Gravity of earth is ", gravity);
+console.log(`Gravity of earth is ${gravity}`);
+console.log(`Boiling point waterof is ${boilingPoint}`);
